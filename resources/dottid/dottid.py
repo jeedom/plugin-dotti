f82664761956e13ece7df6d9599d350dbab30faf
Loïc <loic@jeedom.com>
2016-08-12 17:51:41 +0200

    again

--- resources/dottid/dottid.py
+++ resources/dottid/dottid.py
@@ -102,7 +102,7 @@ def display(mac=None,data=None):
 		return
 	logging.debug('Write display into '+str(mac))
 	for pixel, value in data.iteritems():
-		write(mac,struct.pack('<BBBBBB', 0x07, 0x02,int(pixel), int(value[0]), int(value[1]), int(value[2])))
+		write(mac,struct.pack('<BBBBBB', 0x07, 0x02,int(pixel), int(value['0']), int(value['1']), int(value['2'])))
 		time.sleep(0.05)
 
 def loadid(mac=None,loadid=None):
