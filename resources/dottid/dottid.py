9f8f7bc034088deacf79a910c58ede8fcd7fcd9e
Loïc <loic@jeedom.com>
2016-08-18 21:00:55 +0200

    bugfix

--- resources/dottid/dottid.py
+++ resources/dottid/dottid.py
@@ -165,9 +165,9 @@ def color(mac=None,data=None):
 
 	for i in range(64): 
 		if '0' in data:
-			DOTTIS[mac]['display'][int(i)] = rgb_to_hex((int(value['0']), int(value['1']), int(value['2'])))
+			DOTTIS[mac]['display'][int(i) + 1] = rgb_to_hex((int(value['0']), int(value['1']), int(value['2'])))
 		if 0 in data:
-			DOTTIS[mac]['display'][int(i)] = rgb_to_hex((data[0], data[1], data[2]))
+			DOTTIS[mac]['display'][int(i) + 1] = rgb_to_hex((data[0], data[1], data[2]))
 
 def loadid(mac=None,loadid=None):
 	global DOTTIS
