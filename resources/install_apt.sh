9ab4436692167de73b019bb39822f204262c4b3f
Loïc <loic@jeedom.com>
2017-03-20 15:17:58 +0100

    Begin update dependancy installation script

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,13 +1,17 @@
-touch /tmp/dependancy_dotti_in_progress
-echo 0 > /tmp/dependancy_dotti_in_progress
+PROGRESS_FILE=/tmp/dependancy_dotti_in_progress
+if [ ! -z $1 ]; then
+	PROGRESS_FILE=$1
+fi
+touch ${PROGRESS_FILE}
+echo 0 > ${PROGRESS_FILE}
 echo "Launch install of Dotti dependancy"
 sudo apt-get update
-echo 50 > /tmp/dependancy_dotti_in_progress
+echo 50 > ${PROGRESS_FILE}
 sudo apt-get install -y bluez bluez-hcidump 
-echo 66 > /tmp/dependancy_dotti_in_progress
+echo 66 > ${PROGRESS_FILE}
 sudo apt-get install -y libglib2.0-dev
-echo 75 > /tmp/dependancy_dotti_in_progress
+echo 75 > ${PROGRESS_FILE}
 sudo pip install bluepy
-echo 100 > /tmp/dependancy_dotti_in_progress
+echo 100 > ${PROGRESS_FILE}
 echo "Everything is successfully installed!"
-rm /tmp/dependancy_dotti_in_progress
+rm ${PROGRESS_FILE}
